b1fcf7e1303227ec256615ec27957da7aad0405d
Prionto80 <153057928+Prionto80@users.noreply.github.com>
2024-09-09 03:09:39 +0600

    Update bm.py

--- bm.py
+++ bm.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, send_file
+from flask import Flask, render_template, redirect, url_for, request, send_file, jsonify
 from CreateBill import CreateBill
 
 app = Flask(__name__)
@@ -16,8 +16,8 @@ def document():
 		showroom=request.form['showroom']
 		invoice = CreateBill(
 			billNo=bill_no,
-			brand = showroom,
-			showroom = request.form['showroom'],
+			brand = request.form['brand'],
+			showroom = showroom,
 			address = request.form['address'],
 			mobile = request.form['mobile'],
 			board = request.form['board']
@@ -25,9 +25,32 @@ def document():
 
 		invoice_file = invoice.save_to_bytes()
 		return send_file(invoice_file, as_attachment=True, download_name=f"{bill_no} {showroom}.docx", mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+		return redirect(url_for("index"))
 
 	else:
 		return render_template('new.html')
+
+@app.route('/submit', methods=["POST"])
+def mobile():
+	data = request.get_json()
+	sno = data.get('sno')
+	brand = data.get('brand')
+	show = data.get('show')
+	addr = data.get('addr')
+	mob = data.get('mob')
+	board = data.get('board')
+	invoice = CreateBill(
+			billNo = sno,
+			brand = brand,
+			showroom = show,
+			address = addr,
+			mobile = mob,
+			board = board,
+		)
+	invoice_file = invoice.save_to_bytes()
+	return send_file(invoice_file, as_attachment=True, download_name=f"{bill_no} {showroom}.docx", mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+	return redirect(url_for("index"))
+
 
 
 if __name__ == "__main__":
